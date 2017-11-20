307984acf5e5ff6623e4ce20c937048e40d8d3c5
maxLoveCode <maxkidy@gmail.com>
2017-11-20 18:05:52 +0800

    ckeditor

--- src/main/java/com/supply/stock/stockManagement/controller/WarehouseController.java
+++ src/main/java/com/supply/stock/stockManagement/controller/WarehouseController.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -64,8 +67,10 @@ public class WarehouseController extends BaseController {
 	
 	@ResponseBody
 	@RequestMapping(value = "/imageUpload")
-	public Map<String, Object> imageUpload()
+	public Map<String, Object> imageUpload(MultipartFile file, HttpServletRequest request)
 	{
+		System.out.println(request);
+		System.out.println(file);
 		Map<String, Object> result = new HashMap<String, Object>();
 		result.put("uploaded", 1);
 		result.put("fileName", "filename");
